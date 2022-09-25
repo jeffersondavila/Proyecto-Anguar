4340e959eab72ef47bd606d8ec35b0b50495f4e2
jeffersondavila <jeffersondavila16@gmail.com>
2022-09-25 09:42:18 -0600

    [ADD] Se agregarón los direccionamientos del resto de mantenimientos en el nav, a excepción de los Usuarios

--- src/app/material/material-routing.module.ts
+++ src/app/material/material-routing.module.ts
@@ -2,15 +2,52 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AsignacionAlumnoComponent } from '../asignaciones/pages/asignacion-alumno/asignacion-alumno.component';
 import { AsignacionProfesorComponent } from '../asignaciones/pages/asignacion-profesor/asignacion-profesor.component';
+import { CrearUsuarioComponent } from '../login/Crear/crear.component';
 import { CarreraComponent } from '../mantenimientos/pages/carrera/carrera.component';
+import { CursoComponent } from '../mantenimientos/pages/curso/curso.component';
+import { FacultadComponent } from '../mantenimientos/pages/facultad/facultad.component';
+import { MaestroComponent } from '../mantenimientos/pages/maestro/maestro.component';
+import { PerfilComponent } from '../mantenimientos/pages/perfil/perfil.component';
+import { SeccionComponent } from '../mantenimientos/pages/seccion/seccion.component';
+import { SedeComponent } from '../mantenimientos/pages/sede/sede.component';
+import { ExamenComponent } from '../mantenimientos/pages/examen/examen.component';
+import { NotaComponent } from '../mantenimientos/pages/nota/nota.component';
 
-const routes:Routes=[
+const routes: Routes = [
   {
-    path:'',
-    children:[
+    path: '',
+    children: [
       {
         path: 'carrera',
         component: CarreraComponent
+      },
+      {
+        path: 'curso',
+        component: CursoComponent
+      },
+      {
+        path: 'facultad',
+        component: FacultadComponent
+      },
+      {
+        path: 'maestro',
+        component: MaestroComponent
+      },
+      {
+        path: 'perfil',
+        component: PerfilComponent
+      },
+      {
+        path: 'seccion',
+        component: SeccionComponent
+      },
+      {
+        path: 'sede',
+        component: SedeComponent
+      },
+      {
+        path: 'usuarios',
+        component: CrearUsuarioComponent
       },
       {
         path: 'asignacionAlumno',
@@ -19,6 +56,14 @@ const routes:Routes=[
       {
         path: 'asignacionProfesor',
         component: AsignacionProfesorComponent
+      },
+      {
+        path: 'examenes',
+        component: ExamenComponent
+      },
+      {
+        path: 'notas',
+        component: NotaComponent
       }
     ]
   }
