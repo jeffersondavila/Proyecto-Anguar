930a0e0f08bf0c43a6786eacc1a857c18c734963
jeffersondavila <jeffersondavila16@gmail.com>
2022-10-21 00:02:58 -0600

    [ADD] Se agrego el crud en el componente de carrera

--- src/app/mantenimientos/pages/carrera/carrera.component.ts
+++ src/app/mantenimientos/pages/carrera/carrera.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { interfaceCarrera } from '../interfaces/admin.interface';
 import { MantenimientoServiciosService } from '../services/mantenimiento-servicios.service';
 
@@ -8,48 +10,52 @@ import { MantenimientoServiciosService } from '../services/mantenimiento-servici
   styleUrls: ['./carrera.component.css']
 })
 export class CarreraComponent {
-  title:string='Mantenimiento Carrera';
-  codigo:string='Codigo carrera';
-  codigoFacultad:string='Codigo facultad';
-  nombre:string='Nombre';
-  estado:string='Email';
+  title: string = 'Mantenimiento Carrera';
+  codigo: string = 'Codigo carrera';
+  codigoFacultad: string = 'Codigo facultad';
+  nombre: string = 'Nombre';
+  estado: string = 'Email';
 
-  // @Input() registroCarrera: interfaceCarrera = {
-  //   codigo: '',
-  //   codigoFacultad: '',
-  //   nombre: '',
-  // }
-  // @Output() onNewUser: EventEmitter<interfaceCarrera> = new EventEmitter();
-  // ingresar() {
-  //   if (this.registroCarrera.nombre.trim().length === 0) {
-  //     return;
-  //   }
-  //   console.log(this.registroCarrera);
-  //   this.onNewUser.emit(this.registroCarrera)
-  //   this.registroCarrera = {
-  //     codigo: '',
-  //     codigoFacultad: '',
-  //     nombre: '',
-  //   }
-  // }
+  carrera: interfaceCarrera[] = [];
 
-  // Tabla de datos
-  // @Input() tablaCarreras: interfaceCarrera[] = [];
+  addCarrera: interfaceCarrera = {
+    codigoFacultad: 0,
+    nombre: '',
+    estado: 1,
+  }
 
-  // // Intersección datos
-  // carreras: interfaceCarrera[] = [];
-  // agregarNuevaCarrera(argumento: interfaceCarrera) {
-  //   this.carreras.push(argumento);
-  // }
-  // constructor(private ingresoServ: ServiciosService) {
-  //   this.carreras = this.ingresoServ.serviceCarreras;
-  // }
-
-  //Llenado de tabla
-  carrera:interfaceCarrera[]=[];
-  constructor(private carreraService: MantenimientoServiciosService) { }
+  constructor(private carreraService: MantenimientoServiciosService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.carreraService.getCarrera().subscribe(carrera=>this.carrera = carrera);
+    //Llenado de tablaa
+    this.carreraService.getCarrera().subscribe(carrera => this.carrera = carrera);
+  }
+
+  guardar() {
+    if (this.addCarrera.id) {
+      //actualizar
+      this.carreraService.actualizarCarrera(this.addCarrera)
+        .subscribe(addCarrera => console.log('Actualizando...', addCarrera))
+    } else {
+      //crear
+      // this.carreraService.agregarCarrera(this.addCarrera)
+      //   .subscribe(addCarrera => {
+      //     this.router.navigate(['/mantenimientos/carrera', addCarrera.id]);
+      //   })
+      this.carreraService.agregarCarrera(this.addCarrera)
+        .subscribe(addCarrera => console.log('Agregando...', addCarrera.id))
+    }
+  }
+
+  borrar() {
+    this.carreraService.eliminar(this.addCarrera.id!)
+      .subscribe(resp => {
+        this.router.navigate(['/mantenimientos/carrera'])
+      });
+    // this.carreraService.getCarrera().subscribe(carrera => this.carrera = carrera);
+  }
+
+  cambios() {
+    this.carreraService.getCarrera().subscribe(carrera => this.carrera = carrera);
   }
 }
--- src/app/mantenimientos/pages/services/mantenimiento-servicios.service.ts
+++ src/app/mantenimientos/pages/services/mantenimiento-servicios.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { interfaceFacultad, interfaceAlumno, interfaceCarrera, interfaceCurso, interfaceExamenes, interfaceProfesor, interfaceNotas, interfacePerfil, interfaceSeccion, interfaceSede } from '../interfaces/admin.interface';
 
 @Injectable({
@@ -9,6 +10,7 @@ export class MantenimientoServiciosService {
 
   constructor( private http:HttpClient ) { }
 
+  // Llenar listas
   getAlumno(){
     return this.http.get<interfaceAlumno[]>('http://localhost:3000/alumno')
   }
@@ -38,5 +40,19 @@ export class MantenimientoServiciosService {
   }
   getSede(){
     return this.http.get<interfaceSede[]>('http://localhost:3000/sede')
+  }
+
+  // Ingresar datos a la bd
+  agregarCarrera(carrera:interfaceCarrera):Observable<interfaceCarrera>{
+    return this.http.post<interfaceCarrera>('http://localhost:3000/carrera',carrera)
+  }
+  actualizarCarrera(carrera:interfaceCarrera):Observable<interfaceCarrera>{
+    return this.http.put<interfaceCarrera>(`http://localhost:3000/carrera/${carrera.id}`,carrera)
+  }
+  getPorId(id:number):Observable<interfaceCarrera>{
+    return this.http.get<interfaceCarrera>(`http://localhost:3000/carrera/${id}`);
+  }
+  eliminar(id:number):Observable<any>{
+    return this.http.delete<any>(`http://localhost:3000/carrera/${id}`);
   }
 }
